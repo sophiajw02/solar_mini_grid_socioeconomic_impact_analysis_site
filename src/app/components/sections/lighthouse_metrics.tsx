768c8efa830436524304e04ad941daf0a782a92c
Wu, Sophia Jiaxin <sophiajwu02@gmail.com>
2024-09-22 14:25:14 -0400

    update with lighthouse metrics
    check through google elements in incognito

--- src/app/components/sections/lighthouse_metrics.tsx
+++ src/app/components/sections/lighthouse_metrics.tsx
@@ -3,13 +3,25 @@ import { ReactNode } from 'react';
 
 const Lighthouse = (): ReactNode => {
     return (
-        <div className="py-2" id="lighthouse">
-          <div className="text-xl font-bold pb-2">Lighthouse Scores</div>
-          <p>Performance: </p>
-          <p>Accessibility: </p>
-          <p>Best Practices: </p>
-          <p>SEO: </p>
+      <>
+        <div className="text-xl font-bold pb-2">Lighthouse Scores</div>
+        <div className="columns-2 pb-2" id="lighthouse">
+          <div className="text-gray-600">
+            <p className="text-black text-lg font-semibold">Desktop </p>
+            <p>Performance: 100</p>
+            <p>Accessibility: 100</p>
+            <p>Best Practices: 100</p>
+            <p>SEO: 100</p>
+          </div>
+          <div className="text-gray-600">
+          <p className="text-black text-lg font-semibold">Mobile </p>
+            <p>Performance: 99 </p>
+            <p>Accessibility: 100</p>
+            <p>Best Practices: 100</p>
+            <p>SEO: 100</p>
+          </div>
         </div>
+        </>
     );
 };
 
