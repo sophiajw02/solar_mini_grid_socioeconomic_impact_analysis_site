c5a785bcbde4cec79d7bd97a7730e8f34e518033
Wu, Sophia Jiaxin <sophiajwu02@gmail.com>
2024-09-22 14:04:53 -0400

    deploy update: issue with apostrophe escape

--- src/app/components/sections/about.tsx
+++ src/app/components/sections/about.tsx
@@ -12,7 +12,7 @@ const About = (): ReactNode => {
               <div className="text-xl font-bold pb-2">Project Description</div>
               <p>Earlier this year in 2024, Renewvia published the first-ever peer-reviewed, comprehensive socioeconomic impact analysis for mini-grids in rural Africa.
                 The data is open-source, and the statistical tools used in the paper to draw conclusions are all very general. Little attention was paid to subgroup analyses or correlations in results.
-                There is great potential for follow-up analyses to ask and answer interesting questions related to the impact of Renewvia's work on communities and how better understanding that impact could influence the engineering design process.
+                There is great potential for follow-up analyses to ask and answer interesting questions related to the impact of Renewvia&aposs work on communities and how better understanding that impact could influence the engineering design process.
                 </p>
             </div>
             <div className="py-2">
